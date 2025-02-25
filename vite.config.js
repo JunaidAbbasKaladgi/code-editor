02a76ec5fea04942624f13dfc65982dc5829b6fe
JunaidAbbas <junaidabbaskaladgi@gmail.com>
2025-02-25 16:23:55 +0530

    Updated Vite config for EC2 deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure the base path is set correctly
+  server: {
+    host: true, // Allow access from external sources (EC2)
+    port: 3000, // Or use 80 for direct access
+  }
 })
